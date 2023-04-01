d143283774aff07b5abf97bb785249a8865a747b
BartolomeoGonzalo <bartolomeogonzalo@gmail.com>
2023-04-01 20:57:44 -0300

    cambios general

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const burguer = document.querySelector('#burguer');
 const menu = document.querySelector('#menu-hamburguesa div:nth-child(2)');
+const itemsMenu = document.querySelector('#menu-hamburguesa div:last-child');
+
 burguer.addEventListener('click', e=>{
+    burguer.classList.toggle('active');
+    menu.classList.toggle('open');
+});
+
+itemsMenu.addEventListener('click', e=>{
     burguer.classList.toggle('active');
     menu.classList.toggle('open');
 });
